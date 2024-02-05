f122a9f3fef7ea4d2dcaf78125ac01a61c2cf82b
Oery <48154940+Oery@users.noreply.github.com>
2024-02-05 16:59:04 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# ds2-mad-warrior-alert
- Script to farm Mad Warriors easily
+# Dark Souls II Mad Warrior Detection Script
+
+## Overview
+
+This script is designed to detect the presence of the Mad Warrior in Belfry Sol. Utilizing memory usage monitoring, the script identifies when the Mad Warrior spawns by detecting spikes in the game's memory usage. This approach is based on the unique increase in memory caused by the game loading the Mad Warrior's textures and model. When detected, the script will alert the player with a sound, enabling a more efficient farming experience.
+
+## Features
+
+-   **Audio Alerts:** Plays a sound alert when the Mad Warrior is detected.
+-   **Real-time Memory Monitoring:** Continuously checks the memory usage of the Dark Souls II process.
+-   **Automatic Detection:** Identifies memory spikes indicative of the Mad Warrior's presence.
+
+## Prerequisites
+
+Before running the script, ensure you have Python installed on your system. This script is compatible with Python 3.x and has been tested on Windows.
+
+## Installation
+
+1. **Clone the Repository**
+
+-   Use Git to clone the repository to your local machine, or download the ZIP file directly from GitHub.
+
+2. **Install Dependencies**
+
+-   Navigate to the script's directory in your terminal or command prompt.
+-   Run the following command to install the required Python libraries:
+
+```sh
+pip install -r requirements.txt
+```
+
+## Usage
+
+To start monitoring Dark Souls II for the Mad Warrior spawn, follow these steps:
+
+1. **Ensure Dark Souls II is Running**
+
+-   Launch Dark Souls II and enter the game world.
+
+2. **Run the Script**
+
+-   Open a terminal or command prompt window.
+-   Navigate to the directory where the script is located.
+-   Run the script with Python:
+
+```sh
+python mad_warrior_alert.py
+```
+
+3. **Monitor for Detection**
+
+-   Keep resting and quitting the bonfire until an alert pops up
+-   Note that with the default settings, teleporting using the feather will cause a false alert
+-   You can change your Windows Terminal settings to set it as always on top. With it the window won't go away when you're focusing the game
+
+## Configuration
+
+-   **Process Name:** By default, the script monitors "DarkSoulsII.exe"
+-   **Refresh Rate:** Set to 0.5 seconds by default. Adjust for more or less frequent checks.
+-   **Memory Threshold:** A lower threshold will result in less false positives during loading screen but will also result in less ennemies detected.
+-   **Sound Alert:** The path to the sound file for alert
---
+++ mad_warrior_alert.py
@@ -0,0 +1,82 @@
+import psutil
+import os
+import time
+import sys
+import threading
+import playsound
+
+PROCESS_NAME = "DarkSoulsII.exe"  # Process name to monitor
+REFRESH_RATE = 0.5  # Refresh rate in seconds
+THRESHOLD = 5  # Memory spike threshold in MB
+SOUND_ALERT_ENABLED = True  # Enable or disable the sound alert
+ALERT_PATH = "./mad_warrior_alert.mp3"  # Path to the alert sound file
+
+def play_alert_sound():
+    """Play a sound to alert the user of a memory spike."""
+    try:
+        if SOUND_ALERT_ENABLED: playsound.playsound(ALERT_PATH)
+    except Exception as e:
+        pass
+        
+def clear_console():
+    """Clear the console."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_memory_usage(process):
+    """Get memory usage of a process."""
+    try:
+        memory_usage = process.memory_info().rss / 1024 ** 2  # Convert bytes to MB
+        return memory_usage
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        print(f"Process {process.name()} no longer exists.")
+        sys.exit(1)
+        
+def find_process_by_name(process_name):
+    """Find a process by its name."""
+    for proc in psutil.process_iter(attrs=['pid', 'name']):
+        if proc.info['name'] == process_name:
+            return proc
+    return None
+
+def monitor_memory_usage():
+    """Monitor the memory usage and display a message if there's a spike."""
+    
+    process = None
+    while process is None:
+        process = find_process_by_name(PROCESS_NAME)
+        if process is None:
+            clear_console()
+            print(f"Waiting for process {PROCESS_NAME} to start...")
+            time.sleep(1)  # Wait a bit before trying again
+        else:
+            clear_console()
+            print(f"Found process {PROCESS_NAME}, starting memory usage monitoring.")
+            time.sleep(1)  # Wait a bit before starting the monitoring
+    
+    prev_memory_usage = get_memory_usage(process)
+    spike_message_displayed_until = 0 # Timestamp until which the alert is displayed
+    
+    while True:
+        current_time = time.time()
+        current_memory_usage = get_memory_usage(process)
+        
+        current_memory_usage_int = int(current_memory_usage) # Convert to an integer for display
+        
+        if current_time < spike_message_displayed_until:
+            threading.Thread(target=play_alert_sound, daemon=True).start()
+            clear_console()
+            print(f"Current Memory Usage: {current_memory_usage_int} Mo")
+            print("/!\ Mad Warrior Detected /!\\")
+            time.sleep(5)
+        elif current_memory_usage != prev_memory_usage:
+            clear_console()
+            print(f"Current Memory Usage: {current_memory_usage_int} Mo")
+            
+        if current_memory_usage - prev_memory_usage > THRESHOLD:
+            spike_message_displayed_until = current_time + 5  # Set the future timestamp to stop displaying the alert
+                
+        prev_memory_usage = current_memory_usage
+        time.sleep(0.5)  # Refresh rate
+
+if __name__ == "__main__":
+    monitor_memory_usage()
